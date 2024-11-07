26ddbe6e298b0782aa0565a503c349b9b06d131e
Agslz <agsp3rs3o@gmail.com>
2024-11-06 22:58:20 -0300

    Update PrincipalFaqs.tsx

--- src/app/pages/faq/PrincipalFaqs.tsx
+++ src/app/pages/faq/PrincipalFaqs.tsx
@@ -1,26 +1,12 @@
 'use client';
 
 import React, { useState } from 'react';
-import CustomButton from '../../components/Button';
+import BlackButton from '../../components/BlackButton';
+import Button from '../../components/Button';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-interface ButtonProps {
-  label: string;
-  onClick: () => void;
-  fullWidth?: boolean;
-  className?: string;
-}
-
-const Button: React.FC<ButtonProps> = ({ label, onClick, fullWidth = false, className = '' }) => {
-  return (
-    <button
-      className={`${fullWidth ? 'w-full' : 'w-auto'} p-4 font-bold rounded-md shadow-md hover:bg-gray-200 ${className}`}
-      onClick={onClick}
-    >
-      {label}
-    </button>
-  );
-};
 
 const faqs = [
   { question: '¿Qué es W3?', answer: 'W3 es una tarjeta prepaga.' },
@@ -38,9 +24,15 @@ const PrincipalFaqs = () => {
   };
 
   return (
-    <div className="relative w-[360px] h-[800px]  flex flex-col items-center p-5">
+    <div className="relative w-[360px] h-[800px] flex flex-col items-center p-5">
+
+<div className="w-full flex items-center mb-4 text-[#FEF7FF]">
+        <ArrowBackIcon className="mr-2" /> {/* Icono de flecha hacia la izquierda */}
+        <span>Volver</span>
+      </div>
+
       <div className="w-full flex flex-row items-center mb-5">
-        <ArrowDropDownIcon className="text-white" />
+        <HelpOutlineIcon className="text-white" />
         <h2 className="text-white font-lato text-lg ml-2">Preguntas frecuentes</h2>
       </div>
       <h3 className="text-white text-xl font-bold mb-5">¿Tenés alguna pregunta?</h3>
@@ -52,9 +44,7 @@ const PrincipalFaqs = () => {
               onClick={() => toggleFaq(index)}
             >
               <span className="font-bold text-black">{faq.question}</span>
-              <ArrowDropDownIcon
-                className={`transition-transform ${activeIndex === index ? 'rotate-180' : ''}`}
-              />
+              <ArrowDropDownIcon className='text-black'/>
             </div>
             {activeIndex === index && (
               <p className="mt-3 text-gray-700">{faq.answer}</p>
@@ -63,12 +53,12 @@ const PrincipalFaqs = () => {
         ))}
       </div>
       <div className="w-full flex flex-col items-center gap-4 mt-20">
-        <CustomButton
+        <BlackButton
           label="Busca más preguntas acá"
           onClick={() => console.log('Busca más preguntas acá')}
           fullWidth={true}
         />
-        <CustomButton
+        <Button
           label="Volver al inicio"
           onClick={() => console.log('Volver al inicio')}
           fullWidth={true}
