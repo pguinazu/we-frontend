ab8b400fea16ade42bd56f210658abfb27965acd
Agslz <agsp3rs3o@gmail.com>
2024-11-08 17:09:36 -0300

    Update HomeScreen.tsx

--- src/app/pages/HomeScreen.tsx
+++ src/app/pages/HomeScreen.tsx
@@ -3,76 +3,109 @@
 import React from 'react';
 import Title from '../components/Title';
 import Subtitle from '../components/Subtitle';
-import Button from '../components/Button';
+import BlackButton from '../components/BlackButton';
+import OutboxIcon from '@mui/icons-material/Outbox';
+import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Image from 'next/image';
 
-export default function HomePage() {
+export default function  HomePage() {
   const handleReceiveClick = () => {
-    // Lógica para recibir fondos
     console.log('Recibir fondos');
   };
 
   const handlePauseCardClick = () => {
-    // Lógica para pausar tarjeta
     console.log('Pausar tarjeta');
   };
 
   return (
-    <main className="flex flex-col items-center min-h-screen bg-gradient-to-t from-[#434343] via-[#232323] to-black text-white">
+    <main className="flex flex-col items-center min-h-screen text-white relative">
       
-      {/* Status bar */}
-      <div className="flex justify-between items-center px-8 py-3 w-full">
-        <span className="text-sm font-roboto">9:30</span>
-        <div className="flex space-x-1">
-          {/* Icon placeholders for signal, wifi, and battery */}
-          <span className="w-4 h-4 bg-white opacity-40 rounded-full"></span>
-          <span className="w-4 h-4 bg-white opacity-40 rounded-full"></span>
-          <span className="w-4 h-4 bg-white opacity-40 rounded-full"></span>
-        </div>
-      </div>
-
-      {/* User Greeting */}
-      <div className="flex items-center px-8 mt-2 w-full">
+      {/* Saludo personalizado para el usuario */}
+      <div className="flex items-center px-8 mt-4 w-full">
         <div className="bg-[#EADDFF] text-[#4F378A] flex items-center justify-center rounded-full w-8 h-8">
           A
         </div>
-        <span className="text-sm font-roboto ml-2">Hola {`{UserName}`}</span>
+        <span className="text-sm ml-2">Hola {`{UserName}`}</span>
       </div>
 
-      {/* Card Section */}
-      <div className="relative w-[296px] bg-gradient-to-r from-black to-[#434343] rounded-lg p-4 shadow-lg mt-4">
-        <div className="flex justify-between items-center mb-4">
-          <Title text="VISA" />
-          {/* <Image src="/we-icon.png" alt="WE Logo" width={20} height={20} /> */}
+      {/* Sección de la tarjeta principal */}
+      <div className="relative w-[296px] bg-gradient-to-r from-black to-[#434343] rounded-lg p-6 shadow-lg mt-6 z-10">
+        <div className="flex justify-between items-center mb-2">
+          {/* Iconos de la tarjeta */}
+          <div className="flex items-center gap-2">
+            <Image src="/icons/VisaIcon.png" alt="Visa Icon" width={50} height={20} />
+          </div>
+          <Image src="/icons/WeCard.png" alt="We Card" width={35} height={35} className="opacity-60" />
         </div>
-        <div className="text-lg font-semibold">**** 4569</div>
-        <Subtitle text="Saldo disponible:" />
-        <div className="text-2xl font-bold">USD 0,00</div>
+        <div className="text-lg font-semibold tracking-wider">**** 4569</div>
+        
+        {/* Saldo disponible en la tarjeta */}
+        <div className="flex items-center justify-between mt-6">
+          <div>
+            <p className="text-xs font-normal">Saldo disponible:</p>
+            <div className="text-2xl font-bold text-left">USD 0,00</div>
+          </div>
+          {/* Botón de navegación */}
+          <div className='pt-6'>
+          <div className="flex items-center justify-center w-[44px] h-[24px] bg-[#FEF7FF] rounded-full p-[4px_4px]">
+          <NavigateNextIcon className="text-gray-800" style={{ width: '24px', height: '24px' }} />
+          </div>
+          </div>
+        </div>
       </div>
-        <div className="flex mt-4 space-x-4">
-          <Button label="Recibir" fullWidth onClick={handleReceiveClick} />
-          <Button label="Pausar tarjeta" fullWidth onClick={handlePauseCardClick} />
-        </div>
 
-      {/* Empty Transactions Section */}
-      <div className="flex flex-col items-center mt-8">
-        <div className="bg-white p-4 rounded-full mb-4">
-          {/* <Image src="/outbox-icon.png" alt="Outbox Icon" width={32} height={32} /> */}
-        </div>
-        <Title text="Aún no hay movimientos" />
-        <Subtitle text="Pronto podrás ver los últimos movimientos que realizaste en tu cuenta" />
+      {/* Botones de acción para recibir fondos y pausar tarjeta */}
+      <div className="flex mt-4 space-x-4 w-[296px] z-10">
+      {/* Botón para recibir fondos con fondo negro */}
+      <BlackButton
+        label={<><FileDownloadIcon className="mr-2" />Recibir</>}
+        fullWidth
+        onClick={handleReceiveClick}
+        className="text-sm bg-[#100F0F] text-[#FAFAFA] rounded-md h-[68px]" // Color de fondo negro y texto casi blanco
+      />
+      {/* Botón para pausar tarjeta con fondo gris oscuro */}
+      {/* Falta arreglar que el Pausar tarjeta quede en la misma linea */}
+      <BlackButton
+        label={<><PauseCircleOutlineIcon className="mr-2" />Pausar tarjeta</>}
+        fullWidth
+        onClick={handlePauseCardClick}
+        className="text-sm bg-[#202020] text-[#FAFAFA] rounded-md h-[68px]" // Fondo gris oscuro y texto casi blanco
+      />
+    </div>
+
+      {/* Sección de movimientos vacía */}
+      <div className="flex flex-col items-center mt-8 z-10">
+        <OutboxIcon className="text-white mb-4 !text-[50px]" /> {/* Icono de caja vacía */}
+        <Title text="Aún no hay movimientos" className="text-center text-xl pb-3" />
+        <Subtitle text="Pronto podrás ver los últimos movimientos que realizaste en tu cuenta" className="text-center" />
       </div>
 
-      {/* Where to Use Section */}
-      <div className="flex flex-col items-center mt-8 px-8 w-full">
+      {/* Sección de uso de la tarjeta en comercios */}
+      <div className="flex flex-col items-start mt-8 px-8 w-full z-10">
         <Subtitle text="¿Dónde podes usar la tarjeta?" />
+
+        {/* Fila de íconos de métodos de pago */}
+        {/* Los iconos deberían de cambiarse ya que el background choca con el del icono */}
         <div className="flex justify-between mt-4 w-full">
-          {/* <Image src="/applepay-icon.png" alt="Apple Pay" width={70} height={48} /> */}
-          {/* <Image src="/amazonpay-icon.png" alt="Amazon Pay" width={70} height={48} /> */}
-          {/* <Image src="/paypal-icon.png" alt="PayPal" width={70} height={48} /> */}
-          {/* <Image src="/googlepay-icon.png" alt="Google Pay" width={70} height={48} /> */}
+          <div className="flex items-center justify-center w-[70px] h-[48px]">
+            <Image src="/icons/ApplePayIcon.png" alt="Apple Pay" width={70} height={24} />
+          </div>
+          <div className="flex items-center justify-center w-[70px] h-[48px]">
+            <Image src="/icons/AmazonPayIcon.png" alt="Amazon Pay" width={70} height={24} />
+          </div>
+          <div className="flex items-center justify-center w-[70px] h-[48px]">
+            <Image src="/icons/PayPalIcon.png" alt="PayPal" width={70} height={24} />
+          </div>
+          <div className="flex items-center justify-center w-[70px] h-[48px]">
+            {/* Icono de Google Wallet (comentado) */}
+            <Image src="/icons/PayPalIcon.png" alt="PayPal" width={70} height={24} />
+          </div>
         </div>
-        <Subtitle text="Vas a poder usar tu tarjeta en cualquier negocio físico o virtual que acepte VISA alrededor del mundo. Para más info, ingresa acá" />
+
+        {/* Descripción debajo de los íconos */}
+        <Subtitle text="Vas a poder usar tu tarjeta en cualquier negocio físico o virtual que acepte VISA alrededor del mundo. Para más info, ingresa acá" className="mt-4" />
       </div>
     </main>
   );
