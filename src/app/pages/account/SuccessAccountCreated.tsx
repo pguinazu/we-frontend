f5dc0c02ddae2ecbf2330458d53f500ae0ef838a
Agslz <agsp3rs3o@gmail.com>
2024-11-08 17:13:56 -0300

    Update SuccessAccountCreated.tsx

--- src/app/pages/account/SuccessAccountCreated.tsx
+++ src/app/pages/account/SuccessAccountCreated.tsx
@@ -10,23 +10,30 @@ const SuccessAccountCreated = () => {
     <div className="relative w-[360px] h-[800px] flex flex-col items-center justify-center">
       <div className="flex flex-col items-center gap-5">
         <div className="flex justify-center items-center">
-          <CheckCircleIcon className="text-green-700 rounded-full p-2" style={{ fontSize: '150px' }} />
+          <CheckCircleIcon
+            className="text-green-700"
+            style={{
+              fontSize: '150px',
+            }}
+          />
         </div>
-        <h1 className="text-center text-white font-lato font-bold text-lg leading-7 px-2">
+        <h1 className="text-center text-white font-lato font-bold text-[20px] leading-[28px] w-[241px]">
           ¡Felicitaciones, tu cuenta se ha creado con éxito!
         </h1>
       </div>
-      <div className="flex flex-col items-center gap-5 mt-5">
+      <div className="flex flex-col items-center gap-5 mt-10 w-[296px]">
         <Button
           label="Ingresar a mi cuenta"
           onClick={() => console.log('Ingresar a mi cuenta')}
           fullWidth={true}
+          className="text-black font-semibold shadow-md rounded-md"
         />
         <div className="w-full border border-white rounded-md">
           <BlackButton
             label="Ver preguntas frecuentes"
             onClick={() => console.log('Ver preguntas frecuentes')}
             fullWidth={true}
+            className="text-white font-semibold"
           />
         </div>
       </div>
