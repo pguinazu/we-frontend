4ca49bd3cd8f05f508c7076da86207225e0f534f
Agslz <agsp3rs3o@gmail.com>
2024-11-08 17:13:39 -0300

    Update Title.tsx

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 interface TitleProps {
   text: string;
+  textAlign?: 'left' | 'center' | 'right'; // Propiedad opcional para la alineación
+  className?: string;
 }
 
-const Title: React.FC<TitleProps> = ({ text }) => {
+const Title: React.FC<TitleProps> = ({ text, textAlign = 'center', className }) => {
   return (
-    <h1 className="w-[296px] text-[24px] font-bold leading-[32px] text-center mx-auto font-[var(--FontFamilyGeneral)]">
+    <h1
+      className={`w-[296px] text-[24px] font-bold leading-[32px] mx-auto font-[var(--FontFamilyGeneral)] ${className || ''}`}
+      style={{ textAlign }} // Aplicamos `textAlign` aquí
+    >
       {text}
     </h1>
   );
