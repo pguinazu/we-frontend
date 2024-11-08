3ea9f119de7cd2f688148a939f13a631b03ca910
Agslz <agsp3rs3o@gmail.com>
2024-11-08 17:13:42 -0300

    Update Subtitle.tsx

--- src/app/components/Subtitle.tsx
+++ src/app/components/Subtitle.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 interface SubtitleProps {
   text: string;
+  className?: string; // Agregar className como opcional
 }
 
-const Subtitle: React.FC<SubtitleProps> = ({ text }) => {
+const Subtitle: React.FC<SubtitleProps> = ({ text, className }) => {
   return (
     <p
-      className="w-[296px] text-[14px] font-semibold leading-[24px] text-center mx-auto font-[var(--FontFamilyGeneral)]"
+      className={`w-[296px] text-[14px] font-semibold leading-[24px] text-center mx-auto font-[var(--FontFamilyGeneral)] ${className}`}
       style={{
         color: 'var(--Schemes-Surface, #FEF7FF)',
       }}
