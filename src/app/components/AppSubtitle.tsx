e09603afd842e4866e2a86fae49c9b840b2b7be7
Agslz <agsp3rs3o@gmail.com>
2024-11-14 00:12:45 -0300

    Adding Optional ClassName

--- src/app/components/AppSubtitle.tsx
+++ src/app/components/AppSubtitle.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 interface SubtitleProps {
   text: string;
+  className?: string; // Clase opcional
 }
 
-const Subtitle: React.FC<SubtitleProps> = ({ text }) => {
+const Subtitle: React.FC<SubtitleProps> = ({ text, className }) => {
   return (
     <p
-      className="w-[296px] text-[14px] font-semibold leading-[24px] text-left mx-auto font-[var(--FontFamilyGeneral)]"
+      className={`w-[296px] text-[14px] font-semibold leading-[24px] mx-auto font-[var(--FontFamilyGeneral)] ${className || 'text-left'}`}
       style={{
         color: 'var(--Schemes-Surface, #FEF7FF)',
       }}
