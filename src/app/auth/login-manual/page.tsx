bfb1cf958906f5dd684e5621fe108488c5ff440c
Agslz <agsp3rs3o@gmail.com>
2024-11-28 23:44:30 -0300

    Fixing responsive layout at Login Manual

--- src/app/auth/login-manual/page.tsx
+++ src/app/auth/login-manual/page.tsx
@@ -10,9 +10,8 @@ import { useRouter } from 'next/navigation';
 import { useLogin } from '@/app/contexts/LogInContext';
 import { authService } from '@/app/services/authService';
 
-
 const LoginPage = () => {
-  const { loginData, setLoginData  } = useLogin();
+  const { loginData, setLoginData } = useLogin();
   const [showPassword, setShowPassword] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
   const [emailError, setEmailError] = useState(false);
@@ -21,8 +20,6 @@ const LoginPage = () => {
 
   const router = useRouter();
 
-
-  // Reglas de validación de la contraseña
   const passwordValidationRules = [
     loginData.password.length >= 8 && loginData.password.length <= 16,
     /[A-Z]/.test(loginData.password),
@@ -30,65 +27,72 @@ const LoginPage = () => {
     /[!@#$%^&*(),.?":{}|<>]/.test(loginData.password),
   ];
 
-  // Validación completa de la contraseña
   const isPasswordValid = passwordValidationRules.every((rule) => rule);
 
   const handleLogin = async () => {
-    console.log("Form data:", loginData); // Log para ver los datos en consola
-    try{
+    console.log("Form data:", loginData);
+    try {
       const result = await authService.login(loginData);
       console.log('User Login:', result);
       router.push('/dashboard');
-    }catch(error){
+    } catch (error) {
       console.error('Error Logging user:', error);
-    
     }
-  }
+  };
 
   const togglePasswordVisibility = () => {
     setShowPassword((prev) => !prev);
   };
 
-  // Verifica si ambos campos están llenos y si la contraseña cumple con las reglas para habilitar el botón
   const isFormValid = loginData.userEmail !== '' && loginData.password !== '' && isPasswordValid;
 
   return (
     <div
       className="flex flex-col items-center justify-start min-h-screen px-6"
       style={{
-        background: 'linear-gradient(3.12deg, #000000 3.74%, #232323 79.77%, #434343 124.44%)',
+        background:
+          "linear-gradient(3.12deg, #000000 3.74%, #232323 79.77%, #434343 124.44%)",
       }}
     >
       {/* Encabezado */}
       <div className="w-full max-w-xs mb-4">
-        <div className="flex items-center gap-3 mb-4 px-3 pt-7">
+        <div className="flex items-center gap-3 mb-2 px-2 pt-7">
           <img src="/icons/WeIcon.png" alt="We Icon" className="w-8 h-8" />
           <Title text="Iniciar sesión" textAlign="left" />
         </div>
-        <Subtitle 
-          text="Ingresa tus datos para poder empezar a usar tu tarjeta crypto"
-          textAlign="left" 
-        />
+        <div className="w-full">
+          <Subtitle
+            text="Ingresa tus datos para poder empezar a usar tu tarjeta crypto"
+            textAlign="left"
+          />
+        </div>
       </div>
 
       {/* Formulario */}
-      <div className="w-[296px] bg-[#202020] shadow-md rounded-md flex flex-col gap-6 p-6">
+      <div className="relative w-full max-w-xs p-5 bg-[#202020] shadow-md rounded-md flex flex-col gap-6">
         <TextField
           label="Correo electrónico"
           placeholder="juan@gmail.com"
           variant="filled"
           value={loginData.userEmail}
-          onChange={(e) => setLoginData({ ...loginData, userEmail: e.target.value })}
+          onChange={(e) =>
+            setLoginData({ ...loginData, userEmail: e.target.value })
+          }
           fullWidth
           error={emailError && touched.email}
-          helperText={emailError && touched.email ? "Este correo no se encuentra registrado" : ""}
+          helperText={
+            emailError && touched.email
+              ? "Este correo no se encuentra registrado"
+              : ""
+          }
           InputProps={{
-            style: { backgroundColor: '#FAFAFA' },
-            endAdornment: emailError && touched.email ? (
-              <InputAdornment position="end">
-                <ErrorOutline color="error" />
-              </InputAdornment>
-            ) : null,
+            style: { backgroundColor: "#FAFAFA" },
+            endAdornment:
+              emailError && touched.email ? (
+                <InputAdornment position="end">
+                  <ErrorOutline color="error" />
+                </InputAdornment>
+              ) : null,
           }}
         />
 
@@ -96,23 +100,34 @@ const LoginPage = () => {
           label="Contraseña"
           placeholder="Contraseña"
           variant="filled"
-          type={showPassword ? 'text' : 'password'}
+          type={showPassword ? "text" : "password"}
           value={loginData.password}
-          onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
+          onChange={(e) =>
+            setLoginData({ ...loginData, password: e.target.value })
+          }
           fullWidth
           error={passwordError && touched.password}
-          helperText={passwordError && touched.password ? "La contraseña ingresada no es correcta, volve a intentarlo" : ""}
+          helperText={
+            passwordError && touched.password
+              ? "La contraseña ingresada no es correcta, volve a intentarlo"
+              : ""
+          }
           InputProps={{
-            style: { backgroundColor: '#FAFAFA' },
-            endAdornment: passwordError && touched.password ? (
-              <InputAdornment position="end">
-                <ErrorOutline color="error" />
-              </InputAdornment>
-            ) : (
-              <InputAdornment position="end" onClick={togglePasswordVisibility} style={{ cursor: 'pointer' }}>
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </InputAdornment>
-            ),
+            style: { backgroundColor: "#FAFAFA" },
+            endAdornment:
+              passwordError && touched.password ? (
+                <InputAdornment position="end">
+                  <ErrorOutline color="error" />
+                </InputAdornment>
+              ) : (
+                <InputAdornment
+                  position="end"
+                  onClick={togglePasswordVisibility}
+                  style={{ cursor: "pointer" }}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </InputAdornment>
+              ),
           }}
         />
 
@@ -122,7 +137,7 @@ const LoginPage = () => {
             <Checkbox
               checked={rememberMe}
               onChange={(e) => setRememberMe(e.target.checked)}
-              style={{ color: '#FAFAFA', padding: '0 4px 0 0' }}
+              style={{ color: "#FAFAFA", padding: "0 4px 0 0" }}
               size="small"
             />
             <span>Recordarme</span>
@@ -142,10 +157,13 @@ const LoginPage = () => {
       </div>
 
       {/* Separador de redes sociales */}
-      <Subtitle className='w-full text-center mt-8 mb-4' text="o ingresá con redes sociales"/>
+      <Subtitle
+        className="w-full text-center mt-8 mb-4"
+        text="o ingresá con redes sociales"
+      />
 
+      {/* Botones de redes sociales */}
       <div className="flex flex-col gap-4 w-full max-w-xs">
-        {/* Botón Google */}
         <Button
           label={
             <div className="flex items-center justify-center gap-3 h-full">
@@ -164,7 +182,6 @@ const LoginPage = () => {
           className="flex items-center justify-center"
         />
 
-        {/* Botón Facebook */}
         <Button
           label={
             <div className="flex items-center justify-center gap-3 h-full">
@@ -186,8 +203,11 @@ const LoginPage = () => {
 
       {/* Registro */}
       <div className="text-center mt-6 text-[#FAFAFA] text-[14px]">
-  ¿Aún no tenes cuenta? <a href="/auth/login" className="underline">Regístrate</a>
-</div>
+        ¿Aún no tenes cuenta?{" "}
+        <a href="/auth/login" className="underline">
+          Regístrate
+        </a>
+      </div>
     </div>
   );
 };
