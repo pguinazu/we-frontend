f89309b1ad075fa61884a9236cfedb317ec6dc40
Agslz <agsp3rs3o@gmail.com>
2024-12-02 22:31:13 -0300

    Adding yup to LoginForm

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -5,83 +5,92 @@ import { TextField, IconButton, InputAdornment } from '@mui/material';
 import { Visibility, VisibilityOff, ErrorOutline } from '@mui/icons-material';
 import Button from '../components/Button';
 import { useRouter } from 'next/navigation';
-import { useForm } from '../contexts/SignUpContext'; 
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const LoginForm: React.FC = () => {
-  const { formData, setFormData } = useForm();
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [showPasswordRules, setShowPasswordRules] = useState(false);
-  const [touchedFields, setTouchedFields] = useState({ email: false, password: false, confirmPassword: false });
-  
+
   const router = useRouter();
-  
-  const handleClickShowPassword = () => setShowPassword(!showPassword);
-  const handleClickShowConfirmPassword = () => setShowConfirmPassword(!showConfirmPassword);
 
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newPassword = e.target.value;
-    setFormData({ ...formData, password: newPassword });
-    setShowPasswordRules(true);
-  };
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email('Correo electrónico no válido')
+      .matches(
+        /^[a-zA-Z0-9._%+-]+@(gmail\.com|outlook\.com|yahoo\.com)$/,
+        'El correo debe ser válido y terminar con gmail.com, outlook.com o yahoo.com'
+      )
+      .required('El correo electrónico es obligatorio'),
+    password: Yup.string()
+      .min(8, 'La contraseña debe tener mínimo 8 caracteres')
+      .max(16, 'La contraseña debe tener máximo 16 caracteres')
+      .matches(/[A-Z]/, 'La contraseña debe contener al menos una mayúscula')
+      .matches(/\d/, 'La contraseña debe contener al menos un número')
+      .matches(/[!@#$%^&*(),.?":{}|<>]/, 'La contraseña debe contener al menos un carácter especial')
+      .required('La contraseña es obligatoria'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password')], 'Las contraseñas no coinciden')
+      .required('Confirma tu contraseña'),
+  });
 
-  const handleBlur = (field: string) => {
-    setTouchedFields((prev) => ({ ...prev, [field]: true }));
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log('Form values:', values);
+      router.push('/auth/login-last-step');
+    },
+  });
 
-  const handleContinueClick = () => {
-    console.log("Form data:", formData); // Log para ver los datos en consola
-    router.push('/auth/login-last-step');
-  };
-
-  // Validación del correo electrónico: reemplazar por yup y formik
-
-  // Verificar confirmación de contraseña
-  const isPasswordConfirmed = formData.confirmPassword === formData.password;
-
-  // Reglas de validación de contraseña
+  // Validaciones en tiempo real para la contraseña
   const passwordValidationRules = [
     {
-      label: "La contraseña debe tener mínimo 8 caracteres y máximo 16 caracteres",
-      isValid: formData.password.length >= 8 && formData.password.length <= 16,
+      label: 'La contraseña debe tener mínimo 8 caracteres y máximo 16 caracteres',
+      isValid: formik.values.password.length >= 8 && formik.values.password.length <= 16,
     },
     {
-      label: "La contraseña debe contener al menos una Mayúscula",
-      isValid: /[A-Z]/.test(formData.password),
+      label: 'La contraseña debe contener al menos una mayúscula',
+      isValid: /[A-Z]/.test(formik.values.password),
     },
     {
-      label: "La contraseña debe contener al menos un número",
-      isValid: /\d/.test(formData.password),
+      label: 'La contraseña debe contener al menos un número',
+      isValid: /\d/.test(formik.values.password),
     },
     {
-      label: "La contraseña debe contener al menos un caracter especial",
-      isValid: /[!@#$%^&*(),.?":{}|<>]/.test(formData.password),
+      label: 'La contraseña debe contener al menos un carácter especial',
+      isValid: /[!@#$%^&*(),.?":{}|<>]/.test(formik.values.password),
     },
   ];
 
-  // Determinar si el formulario es válido
-  const isFormValid = isPasswordConfirmed && passwordValidationRules.every(rule => rule.isValid);
-
   return (
-    <div className="relative w-full max-w-xs p-5 bg-[#202020] shadow-md rounded-md flex flex-col gap-6">
+    <form
+      onSubmit={formik.handleSubmit}
+      className="relative w-full max-w-xs p-5 bg-[#202020] shadow-md rounded-md flex flex-col gap-6"
+    >
       <div className="flex flex-col gap-4">
         <TextField
           label="Correo electrónico"
           placeholder="juan@gmail.com"
           variant="filled"
           fullWidth
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-          onBlur={() => handleBlur('email')}
-          error={!touchedFields.email}
-          helperText={!touchedFields.email ? "Correo electrónico no válido" : ""}
-          InputProps={{
-            style: { backgroundColor: '#FAFAFA' },
-            endAdornment: !touchedFields.email ? (
-              <InputAdornment position="end">
-                <ErrorOutline color="error" />
-              </InputAdornment>
-            ) : null,
+          {...formik.getFieldProps('email')}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+          slotProps={{
+            input: {
+              style: { backgroundColor: '#FAFAFA' },
+              endAdornment: formik.touched.email && formik.errors.email ? (
+                <InputAdornment position="end">
+                  <ErrorOutline color="error" />
+                </InputAdornment>
+              ) : null,
+            },
           }}
         />
 
@@ -92,18 +101,22 @@ const LoginForm: React.FC = () => {
             variant="filled"
             type={showPassword ? 'text' : 'password'}
             fullWidth
-            value={formData.password}
-            onChange={handlePasswordChange}
-            onBlur={() => handleBlur('password')}
-            InputProps={{
-              style: { backgroundColor: '#FAFAFA' },
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={handleClickShowPassword}>
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
+            {...formik.getFieldProps('password')}
+            onFocus={() => setShowPasswordRules(true)}
+            onBlur={() => setShowPasswordRules(false)}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+            slotProps={{
+              input: {
+                style: { backgroundColor: '#FAFAFA' },
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
             }}
           />
 
@@ -127,32 +140,32 @@ const LoginForm: React.FC = () => {
           variant="filled"
           type={showConfirmPassword ? 'text' : 'password'}
           fullWidth
-          value={formData.confirmPassword}
-          onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
-          onBlur={() => handleBlur('confirmPassword')}
-          error={!isPasswordConfirmed && touchedFields.confirmPassword}
-          helperText={!isPasswordConfirmed && touchedFields.confirmPassword ? "Las contraseñas no coinciden" : ""}
-          InputProps={{
-            style: { backgroundColor: '#FAFAFA' },
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={handleClickShowConfirmPassword}>
-                  {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            ),
+          {...formik.getFieldProps('confirmPassword')}
+          error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+          helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+          slotProps={{
+            input: {
+              style: { backgroundColor: '#FAFAFA' },
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
+                    {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            },
           }}
         />
       </div>
 
       <Button
         label="Continuar"
-        onClick={handleContinueClick}
+        type="submit"
         fullWidth
-        disabled={!isFormValid}
-        className={!isFormValid ? 'opacity-50 cursor-not-allowed' : ''}
+        disabled={!formik.isValid || formik.isSubmitting}
+        className={!formik.isValid ? 'opacity-50 cursor-not-allowed' : ''}
       />
-    </div>
+    </form>
   );
 };
 
