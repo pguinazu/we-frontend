e4df51c895deb008065ea2c9ccfa0bf081c47f9c
Agslz <agsp3rs3o@gmail.com>
2024-12-19 21:49:07 -0300

    Fixing typo in route

--- src/app/crypto/select-crypto-red/page.tsx
+++ src/app/crypto/select-crypto-red/page.tsx
@@ -35,7 +35,7 @@ const SelectCryptoRed = () => {
       subtitle: `Red ${network.name}`,
       id: network.id,
     });
-    router.push("/cryptoauth/select-crypto-last-step");
+    router.push("/crypto/select-crypto-last-step");
   };
 
   return (
